a5b4bfc0131767fb15d1a0774a4746475fb7fb2d
SirEvanTheThird <evancm369@gmail.com>
2025-03-07 14:22:28 -0500

    BookShelf Half
    Import ArrayList util
    Create char for first letter of book title
    Create ArrayList of the Books on the shelf (max of 8 will be implemented in void method)
    Constructor and a getter for when we want to grab the title letter later

--- BookShelf.java
+++ BookShelf.java
@@ -1,3 +1,17 @@
+import java.util.ArrayList;
+
 public class BookShelf {
+    public char titleLetter;
+    public ArrayList<Book> books;
+
+    public BookShelf(char titleLetter){
+        this.titleLetter = titleLetter;
+        this.books = new ArrayList<>();
+    }
+
+    public char getTitleLetter(){
+        return titleLetter;
+    }
+
     
 }
