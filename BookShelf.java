2367d2233c13389cde57a2bf4c818482294c563e
SirEvanTheThird <evancm369@gmail.com>
2025-03-07 14:38:02 -0500

    BookShelf improve
    Replace titleLetter with shelfLetter (more fitting)
    Create both void methods addBook and remove book
    ToString to print empty of the titles with three spaces between

--- BookShelf.java
+++ BookShelf.java
@@ -1,17 +1,44 @@
 import java.util.ArrayList;
 
 public class BookShelf {
-    public char titleLetter;
+    public char shelfLetter;
     public ArrayList<Book> books;
 
-    public BookShelf(char titleLetter){
-        this.titleLetter = titleLetter;
+    public BookShelf(char shelfLetter){
+        this.shelfLetter = shelfLetter;
         this.books = new ArrayList<>();
     }
 
-    public char getTitleLetter(){
-        return titleLetter;
+    public char getshelfLetter(){
+        return shelfLetter;
     }
 
+    public void addBook(Book book) {
+        //max of 8 books in shelf, also check if title[0] is shelfLetter
+        if (books.size() < 8 && book.title.charAt(0) == shelfLetter) {
+            books.add(book);
+        }
+    }
     
+    public void removeBook(String title){
+        //loop through the books to find title match and remove book at that index
+        for (int i = 0; i< books.size(); i++){
+            if(books.get(i).title.equals(title)){
+                books.remove(i);
+                return; //exit after first removeal, so if removing multiple of same name
+                //then do it multiple times
+            }
+        }
+    }
+    public String toString(){
+        if(books.isEmpty()){
+            return "Empty";
+        }
+
+        String result = "";
+        for(Book book: books) {
+            result += book.toString() + "   "; //three spaces between
+        }
+        return result.trim(); //remove extra spaces at the end
+    }
 }
