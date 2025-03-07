f4e384276ce6ca73345aa750609975131e418d38
SirEvanTheThird <evancm369@gmail.com>
2025-03-07 14:13:08 -0500

    Book Implementation
    Create title and genre string
    Construct title and genre
    ToString to print book title

--- Book.java
+++ Book.java
@@ -1,3 +1,13 @@
 public class Book{
+    public String title;
+    public String genre;
 
+    public Book(String title, String genre){
+        this.title = title;
+        this.genre = genre;
+    }
+
+    public String toString() {
+        return title;
+    }
 }
